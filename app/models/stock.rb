827339652b1a64692a126dd0fb7a8dcb782a1659
Tanner Sigel <tanner2379@live.com>
2020-10-02 09:57:49 -0700

    Added stock model with lookup method using tiingo

---
+++ app/models/stock.rb
@@ -0,0 +1,20 @@
+class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    headers = {"Content-Type" => "application/json", "Authorization" => "Token #{Rails.application.credentials.tiingo[:api_token]}" }
+
+    uri = URI("https://api.tiingo.com/iex/?tickers=" + ticker_symbol.downcase)
+    req = Net::HTTP::Get.new(uri)
+
+    headers.each { |key, value| req[key.to_s] = value}
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    res = http.request(req)
+    response = JSON.parse(res.body)
+
+    response.first
+  end
+
+end
