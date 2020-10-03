8fd254b2824e3577e0525792981ab6922b35fed7
Tanner Sigel <tanner2379@live.com>
2020-10-03 10:36:22 -0700

    Added Friends List View

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def friends_list
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'friends_list', to: 'users#friends_list'
   get 'search_stock', to: 'stocks#search'
 end
